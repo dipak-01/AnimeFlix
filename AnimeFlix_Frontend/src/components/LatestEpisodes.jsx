3eaa319fd1bde3183bb04cd90d53e78812fc06cc
dipak-01 <imt_2022037@iiitm.ac.in>
2024-06-26 15:49:02 +0530

    ui colors change bug

--- AnimeFlix_Frontend/src/components/LatestEpisodes.jsx
+++ AnimeFlix_Frontend/src/components/LatestEpisodes.jsx
@@ -1,6 +1,8 @@
 import CardsType2 from "./CardsType2";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Cards({ name, type, duration, poster,id }) {
+  const [isLoading, setIsLoading] = useState(true); 
   const navigate = useNavigate();
   const handleClick = (id) => {
     navigate(`/anime/info?id=${encodeURIComponent(id)}`);
@@ -9,7 +11,12 @@ function Cards({ name, type, duration, poster,id }) {
     <>
       <div onClick={() => handleClick(id)} className=" max-w-48 min-w-28   sm:h-96 h-64 ">
         <div className="w-full h-4/5">
-          <img className=" h-full w-full rounded-md" src={poster} alt="" />
+        {isLoading && <div className="w-full h-full rounded-t-md bg-gray-900 flex items-center justify-center ">
+                {/* Placeholder or loader here */}
+                <span className="  animate-spin ease-linear rounded-full w-10 h-10 border-t-2 border-b-2 border-orange-500 ml-3"></span>
+              </div>}
+          <img className=" h-full w-full rounded-md" src={poster} alt=""  onLoad={() => setIsLoading(false)}  
+                style={{ display: isLoading ? 'none' : 'block' }}  />
         </div>
         <div className="   text-left  ">
           <p className=" w-3/4 text-md py-1 line-clamp-1 text-clip">{name}</p>
