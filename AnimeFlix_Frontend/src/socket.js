8c6891a3fdcc7c1df558c97fd7b351fbd3dd6650
dipak-01 <imt_2022037@iiitm.ac.in>
2024-08-05 23:22:27 +0530

    refactor: update socket.js to use API_URL for backend server connection

--- AnimeFlix_Frontend/src/socket.js
+++ AnimeFlix_Frontend/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 const API_URL = `${import.meta.env.VITE_BACKEND_SERVER_PORT}`;
 
 // const URL =   "ws://localhost:3000";
-const URL = "ws://localhost:3000" || API_URL;
+const URL =   API_URL;
 
 export const socket = io(URL);
