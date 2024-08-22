85d82b6ee1c3b48843062b1bff5a11fc29aa0445
dipak-01 <imt_2022037@iiitm.ac.in>
2024-08-22 19:06:48 +0530

    fix : user auth password update 2

--- AnimeFlix_Backend/controllers/userController.js
+++ AnimeFlix_Backend/controllers/userController.js
@@ -32,49 +32,37 @@ export const updateUser = async (req, res) => {
     return res.status(401).send("No token provided");
   }
   const token = authHeader.split(" ")[1];
+
   try {
     const decoded = jwt.verify(token, "jwt_secret");
     const userId = decoded.userId;
-    const user = await User.findById(userId).select("-password");
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).send("User not found");
     }
+
     if (userName) user.username = userName;
 
-    if (
-      oldPassword &&
-      newPassword &&
-      !oldPassword === "" &&
-      !newPassword === ""
-    ) {
-      try {
-        const isPasswordCorrect = await bcrypt.compare(
-          oldPassword,
-          user.password
-        );
-        if (!isPasswordCorrect) {
-          return res.status(401).send("Check your Password and Try again");
-        }
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(newPassword, salt);
-        await user.save(); // Assuming user.save() is the method to save the user
-        return res.status(200).send("Password updated successfully");
-      } catch (error) {
-        return res
-          .status(500)
-          .send("An error occurred while updating the password");
+    if (oldPassword && newPassword) {
+      const isPasswordCorrect = await bcrypt.compare(
+        oldPassword,
+        user.password
+      );
+      if (!isPasswordCorrect) {
+        return res.status(401).send("Check your Password and Try again");
       }
-    } else {
-      return res.status(400).send("Old password and new password are required");
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
     }
-    if (req.file) {
-      const fieldName = req.file.fieldname;
-      if (fieldName === "profileImage") {
-        user.avatarUrl = req.file.path;
-      }
+
+    if (req.file && req.file.fieldname === "profileImage") {
+      user.avatarUrl = req.file.path;
     }
+
     await user.save();
-    return res.json(user);
+    const userWithoutPassword = user.toObject();
+    delete userWithoutPassword.password;
+    return res.json(userWithoutPassword);
   } catch (error) {
     console.error(error);
     return res.status(401).send("Invalid or expired token");
