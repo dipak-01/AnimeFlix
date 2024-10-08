b63be72a76f25bbaf585fc968567dd94e88d27dc
Mk281005 <manoj281005@gmail.com>
2024-10-08 23:48:41 +0530

    feat: Added GitHub repo link to the footer

--- AnimeFlix_Frontend/src/components/Footer.jsx
+++ AnimeFlix_Frontend/src/components/Footer.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default () => {
   return (
     <>
       <div className=" bottom-0 mb-0 mt-auto   flex w-full items-center justify-center p-10 text-slate-300">
         <p>
           All right reservers to its corresponding developer <br />
-          Made with ❤️ by dk
+          
+          <a href="https://github.com/dipak-01/AnimeFlix" target='blank'><span  className="ml-1 text-red-600 font-medium">Github<FontAwesomeIcon icon={faGithub} className="ml-3 text-red-600" /></span>
+          </a>
         </p>
       </div>
     </>
