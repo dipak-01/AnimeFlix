9ccb075faabcdc19458b23ad2af1c52cbc484040
dipak-01 <imt_2022037@iiitm.ac.in>
2025-09-16 17:08:12 +0530

    fix: broken animewatch api

--- AnimeFlix_Frontend/src/services/AnimeWatch.js
+++ AnimeFlix_Frontend/src/services/AnimeWatch.js
@@ -11,10 +11,10 @@ export function useFetchData(id) {
       try {
         const res =
           (await axios.get(
-            `${import.meta.env.VITE_ANIME_URL_SECONDARY}/${id}`,
+            `${import.meta.env.VITE_ANIME_URL_SECONDARY}/anime/${id}`,
           )) ||
           (await axios.get(
-            `${import.meta.env.VITE_ANIME_URL}/${id}`,
+            `${import.meta.env.VITE_ANIME_URL}/anime/${id}`,
           ));
         setData(res.data.data);
         setLoading(false);
